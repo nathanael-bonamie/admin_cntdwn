df70fa39df168b9f064e48b5427f25d6ee8b60f8
Nathanael Bonamie <nathbonamie@orange.fr>
2021-09-20 11:39:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ function settings(setup){
 	let dat=new Date();;
 	let form= { day: 'numeric', month: 'short', year: 'numeric'};
 	let xmlhttp = new XMLHttpRequest();
-	let jwtoken = "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";
+	let jwtoken = "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";
 	let sha = sessionStorage.getItem("sha");
 	let content = btoa(setup);
 
